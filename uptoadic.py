d34006c25ca34cfd89b325a821f48444d0412e44
C-Falconer <cfaulkner@anderson.edu>
2023-07-05 16:37:58 -0500

    added more formatting and binary converter

--- uptoadic.py
+++ uptoadic.py
@@ -9,6 +9,8 @@ if len(sys.argv) > 2:
 
 results = []
 for i in range(1, top):
+    if i % 2 == 0:
+        continue
     result = subprocess.run(['python', 'p-adicconv.py', str(i), str(top), str(p)],
             stdout=subprocess.PIPE)
     result = str(result.stdout, 'utf-8').split("\n")
@@ -18,7 +20,12 @@ for i in range(1, top):
 #maximum = len(max(results, key = len))
 results_dict = {}
 for i, result in enumerate(results):
-    results_dict[f"{i + 1}/{top}"] = result.replace('1', "\033[91m1\033[00m")
+    index = i
+    while index % 2 == 0:
+        index += 1
+    #print(sum(map(int, result[:result.find("_")])), end = " ")
+    results_dict[f"{2*i + 1}/{top}"] = result.replace('1', "\033[91m1\033[00m")
 results_dict = dict(sorted(results_dict.items(), key = lambda x:x[1]))
 for key,values in results_dict.items():
+    print(sum(map(int, values.replace("\033[91m1\033[00m", "1")[:values.replace("\033[91m1\033[00m", "1").find("_")])), end=" ")
     print(f"{key}\t{values}")

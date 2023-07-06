236b098ede4215d6c40f6b2d3f25bc449ff82dd3
C-Falconer <cfaulkner@anderson.edu>
2023-07-06 09:26:42 -0500

    updated primes

--- combinedoutput.py
+++ combinedoutput.py
@@ -1,4 +1,5 @@
 import sys, os
+import prime
 
 n = 5
 if len(sys.argv) > 1:
@@ -8,6 +9,7 @@ command = "clear"
 
 for i in range(1, n + 1):
     m = 2*i + 1
+    if prime.isprime(m):
+        command += " && echo -n \"P \""
     command += f" && python binary.py {m} && python uptoadic.py {m}"
-
 os.system(command)
--- prime.py
+++ prime.py
@@ -1,5 +1,10 @@
-def isprime(n):
-    return False
+from math import ceil
 
-if __name__ = '__main__':
+def isprime(n):
+    for i in range(2, ceil(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+if __name__ == '__main__':
     print("Running Prime File")
